ed65b328017eb18507ab416216cf6e224b4abc35
jenya-b <evgeny_rab@inbox.ru>
2023-08-03 13:02:00 +0300

    feat: setting element sizes for chat component

--- src/modules/components/Chat/Chat.styled.ts
+++ src/modules/components/Chat/Chat.styled.ts
@@ -3,11 +3,11 @@ import styled, { css } from 'styled-components';
 import { diamondIcon, infoIcon, inputBtnIcon, messageChatIcon } from 'constants/images';
 import {
   fontStyleMediumBold,
+  fontStyleMediumDemiBold,
   fontStyleSmallBold,
   fontStyleSmallDemiBold,
   fontStyleXSmallNormal,
   fontStyleXXSmallBold,
-  fontStyleXXSmallDemiBold,
 } from 'styles/typography';
 
 export const Wrapper = styled.div`
@@ -17,11 +17,11 @@ export const Wrapper = styled.div`
   border-radius: ${({ theme }) => theme.borders.radiusPrimary}px;
   border: 1px solid ${({ theme }) => theme.colors.borderPrimary};
   display: grid;
-  grid-template: 52px 1fr 80px / 1fr;
+  grid-template: auto 1fr auto / 1fr;
 `;
 
 export const TitleBlock = styled.div`
-  padding: 0 16px;
+  padding: 1.5rem 2rem 0px 2rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -29,8 +29,8 @@ export const TitleBlock = styled.div`
 
 export const Title = styled.h2`
   position: relative;
-  padding-left: 22px;
-  ${fontStyleMediumBold}
+  padding-left: 2.1rem;
+  ${fontStyleMediumDemiBold}
 
   ::before {
     position: absolute;
@@ -38,45 +38,46 @@ export const Title = styled.h2`
     left: 0;
     top: 50%;
     transform: translateY(-50%);
-    width: 18px;
-    height: 18px;
+    width: 1.6rem;
+    height: 1.6rem;
     background: url(${messageChatIcon}) no-repeat;
+    background-size: contain;
     background-position: center;
   }
 `;
 
 export const Info = styled.div`
   position: relative;
-  padding: 5px 10px 4px 26px;
+  padding: 0.41rem 0.72rem 0.4rem 2rem;
   border-radius: ${({ theme }) => theme.borders.radiusSecondary}px;
   background: ${({ theme }) => theme.colors.buttonPrimary};
   ${fontStyleXXSmallBold}
-  letter-spacing: -0.11px;
   text-transform: uppercase;
 
   ::before {
     position: absolute;
     content: '';
-    left: 10px;
+    left: 0.72rem;
     top: 50%;
     transform: translateY(-50%);
-    width: 10px;
-    height: 10px;
+    width: 1rem;
+    height: 1rem;
     background: url(${infoIcon}) no-repeat;
     background-position: center;
+    background-size: contain;
   }
 `;
 
 export const MessageBlock = styled.div`
+  margin-top: 0.85rem;
   background: ${({ theme }) => theme.colors.bgTertiary};
   border-radius: ${({ theme }) => theme.borders.radiusPrimary}px;
-  padding: 16px;
+  padding: 1.2rem 2rem;
   overflow: auto;
 `;
 
 export const ControlBlock = styled.div`
-  margin-top: 3px;
-  padding: 0 17px;
+  padding: 1rem 1.5rem;
   display: flex;
   align-items: center;
 `;
@@ -84,12 +85,12 @@ export const ControlBlock = styled.div`
 export const Label = styled.label`
   position: relative;
   width: 100%;
-  height: 40px;
+  height: 2.8rem;
 `;
 
 export const Input = styled.input`
-  padding-left: 12px;
-  padding-right: 40px;
+  padding-left: 0.85rem;
+  padding-right: 2.9rem;
   width: 100%;
   height: 100%;
   border-radius: ${({ theme }) => theme.borders.radiusSecondary}px;
@@ -97,7 +98,6 @@ export const Input = styled.input`
   color: ${({ theme }) => theme.colors.textPrimary};
   font-family: 'Raleway', sans-serif;
   ${fontStyleSmallDemiBold}
-  letter-spacing: -0.11px;
 `;
 
 export const InputBtn = styled.button`
@@ -105,28 +105,29 @@ export const InputBtn = styled.button`
   right: 15px;
   top: 50%;
   transform: translateY(-50%);
-  width: 20px;
-  height: 20px;
+  width: 1.6rem;
+  height: 1.6rem;
   background: url(${inputBtnIcon}) no-repeat;
   background-position: center;
+  background-size: contain;
 `;
 
 export const listMessageCss = css`
   display: flex;
   flex-direction: column;
-  row-gap: 16px;
+  row-gap: 1.15rem;
 `;
 
 export const MessageWrap = styled.div`
   display: grid;
-  grid-template-columns: 40px 1fr;
-  column-gap: 10px;
+  grid-template-columns: 3rem 1fr;
+  column-gap: 0.72rem;
 `;
 
 export const MessageIcon = styled.div`
   position: relative;
-  width: 40px;
-  height: 40px;
+  width: 3rem;
+  height: 3rem;
   background-color: rgba(255, 255, 255, 0.06);
   border-radius: 50%;
 
@@ -136,8 +137,8 @@ export const MessageIcon = styled.div`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    width: 24px;
-    height: 24px;
+    width: 1.7rem;
+    height: 1.7rem;
     background: url(${diamondIcon.orange}) no-repeat;
     background-position: center;
     background-size: contain;
@@ -147,7 +148,7 @@ export const MessageIcon = styled.div`
 export const MessageInfo = styled.div`
   display: flex;
   flex-direction: column;
-  row-gap: 6px;
+  row-gap: 0.4rem;
 `;
 
 export const UserName = styled.div`
@@ -160,6 +161,6 @@ export const UserPost = styled.div`
 `;
 
 export const TimePost = styled.div`
-  ${fontStyleXXSmallDemiBold}
+  ${fontStyleXSmallNormal}
   opacity: 0.25;
 `;
