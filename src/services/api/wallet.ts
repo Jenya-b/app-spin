012acc7c8ac75b169b2cff6fd4e81290253b8990
jenya-b <evgeny_rab@inbox.ru>
2023-10-10 11:48:31 +0300

    feat: add wallet api

---
+++ src/services/api/wallet.ts
@@ -0,0 +1,52 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+
+interface Wallet {
+  coin: string;
+  address: string;
+  value: number;
+  inGame: number;
+}
+
+interface CreateWalletBtcResponse {
+  result: string;
+  user_id: number;
+  coin: string;
+  address: string;
+}
+
+interface SendBtcRequest {
+  user_id: number;
+  address: string;
+  value: number;
+}
+
+export const userApi = createApi({
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
+  endpoints: (build) => ({
+    getWalletBtc: build.query<Wallet, number>({
+      query: (userId) => ({
+        url: `wallet/btc/get/${userId}`,
+      }),
+    }),
+    createWalletBtc: build.mutation<{ userId: number }, CreateWalletBtcResponse>({
+      query: (userId) => ({
+        method: 'PUT',
+        url: `wallet/btc/create/${userId}`,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }),
+    }),
+    sendBtc: build.mutation<SendBtcRequest, null>({
+      query: (body) => ({
+        method: 'PUT',
+        url: 'wallet/btc/send',
+        body,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }),
+    }),
+  }),
+});
