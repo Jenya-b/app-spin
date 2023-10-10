94e12a19c0f57c920ae10eea40c9e828a7594692
jenya-b <evgeny_rab@inbox.ru>
2023-10-10 12:14:26 +0300

    add chat api

---
+++ src/services/api/chat.ts
@@ -0,0 +1,55 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+
+interface MessageFromChat {
+  user_id: number;
+  text: string;
+  timestamp: string;
+}
+
+interface Message {
+  message_id: number;
+  chat_id: number;
+  user_id: number;
+  text: string;
+  timestamp: string;
+}
+
+export const userApi = createApi({
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
+  endpoints: (build) => ({
+    getMessagesByChatId: build.query<{ [key: string]: MessageFromChat }, number>({
+      query: (chatId) => ({
+        url: `chat/msg_all/${chatId}`,
+      }),
+    }),
+    getMessageById: build.query<Message, number>({
+      query: (messageId) => ({
+        url: `chat/message_id/${messageId}`,
+      }),
+    }),
+    createChat: build.mutation<{ chat_id: number }, { user1_id: number; user2_id: number }>({
+      query: (body) => ({
+        method: 'POST',
+        url: 'chat/create',
+        body,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }),
+    }),
+    addMessage: build.mutation<
+      { chat_id: number; message_id: number },
+      { user1_id: number; text: string; chat_id: number }
+    >({
+      query: ({ chat_id, text, user1_id }) => ({
+        method: 'POST',
+        url: `chat/msg_all/${chat_id}`,
+        body: { text, user1_id },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }),
+    }),
+  }),
+});
