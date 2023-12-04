a21ee950fb472a39b23e31d69fffcbdc913c0971
jenya-b <evgeny_rab@inbox.ru>
2023-12-04 11:58:21 +0300

    feat: remove functionality choose crypto in inGame component

--- src/modules/components/InGame/InGame.tsx
+++ src/modules/components/InGame/InGame.tsx
@@ -2,17 +2,15 @@ import { InGameDataType } from 'data/long';
 import { Wrapper, Count, InfoText, List, Title } from './InGame.styled';
 import { cryptoIcon } from 'constants/images';
 import { useTranslation } from 'react-i18next';
-import { useAppDispatch, useAppSelector } from 'store';
-import { currencySelector, userSelector } from 'store/selectors';
-import { CriptoEnum, changeCurrency } from 'store/reducers/currencySlice';
+import { useAppSelector } from 'store';
+import { userSelector } from 'store/selectors';
+import { CriptoEnum } from 'store/reducers/currencySlice';
 
 interface InGameProps {
   data: InGameDataType[];
 }
 
 export const InGame = ({ data }: InGameProps) => {
-  const dispatch = useAppDispatch();
-  const { currency } = useAppSelector(currencySelector);
   const { balance } = useAppSelector(userSelector);
   const { t } = useTranslation();
 
@@ -23,13 +21,10 @@ export const InGame = ({ data }: InGameProps) => {
         {balance !== null &&
           Object.entries(balance).map(([cryptoName, amount]) => (
             <div
-              onClick={() => dispatch(changeCurrency(cryptoName as CriptoEnum))}
               key={cryptoName}
               style={{
-                background: currency === cryptoName ? 'rgb(40, 41, 67)' : '',
                 padding: '2px',
                 borderRadius: '4px',
-                cursor: 'pointer',
                 textAlign: 'center',
               }}
             >
