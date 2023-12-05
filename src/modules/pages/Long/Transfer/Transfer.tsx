1d8cbbe5361cb54b888960365074d5e7f06fbf75
jenya-b <evgeny_rab@inbox.ru>
2023-12-05 10:51:23 +0300

    feat: add dinamic win param

--- src/modules/pages/Long/Transfer/Transfer.tsx
+++ src/modules/pages/Long/Transfer/Transfer.tsx
@@ -3,6 +3,8 @@ import { useTranslation } from 'react-i18next';
 import { Icon, TransferBlock, TransferCount, TransferInfo, TransferWrap } from './Transfer.styled';
 import { InGameDataType } from 'data/long';
 import { RateSelection } from 'modules/components/RateSelection/RateSelection';
+import { useAppSelector } from 'store';
+import { gameSelector } from 'store/selectors';
 
 interface GameProps {
   data: InGameDataType[];
@@ -10,17 +12,24 @@ interface GameProps {
 
 export const Transfer = ({ data }: GameProps) => {
   const { t } = useTranslation();
+  const {
+    longInfo: { my_bet, round_info },
+  } = useAppSelector(gameSelector);
 
   return (
     <RateSelection data={data}>
       <TransferBlock>
         <TransferWrap>
-          <TransferCount>0.00</TransferCount>
+          <TransferCount>{my_bet !== null ? my_bet.value : '0.00'}</TransferCount>
           <TransferInfo>{t('inRound')}</TransferInfo>
         </TransferWrap>
         <Icon />
         <TransferWrap>
-          <TransferCount>0.00</TransferCount>
+          <TransferCount>
+            {my_bet !== null && round_info !== null
+              ? (my_bet.value * Number(round_info.coef)).toFixed(2)
+              : '0.00'}
+          </TransferCount>
           <TransferInfo>{t('win')}</TransferInfo>
         </TransferWrap>
       </TransferBlock>
